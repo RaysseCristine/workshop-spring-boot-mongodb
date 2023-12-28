a3e5790056da4324aa2996cde9dff8a711722996
Raysse Cristine <rayssecristine@hotmail.com>
2023-12-27 22:29:31 -0300

    Obtendo um post por id

---
+++ src/main/java/com/rayssecristine/workshopmongo/services/PostService.java
@@ -0,0 +1,26 @@
+package com.rayssecristine.workshopmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.rayssecristine.workshopmongo.domain.Post;
+import com.rayssecristine.workshopmongo.repository.PostRepository;
+import com.rayssecristine.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+	
+	@Autowired
+	private PostRepository repo;
+	
+	
+	public Optional<Post> findById(String id) {
+		Optional<Post> user = repo.findById(id);
+		if(user == null) {
+		throw new ObjectNotFoundException("Objeto não encontrado");
+		}
+		return user;
+	}	
+}
